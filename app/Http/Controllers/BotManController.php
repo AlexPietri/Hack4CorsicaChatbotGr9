20d5fe3817f7d8380ef2df0a681b7b325394b38a
Lastennet <lastennet.l@gmail.com>
2018-01-16 09:36:11 +0100

    Update bot controller

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -23,6 +23,11 @@ class BotManController extends Controller
         $botman->hears(' {text}', function (Botman $bot, $text) {
             $bot->types();
 
+            $IDQuestion = DB::table('questions')->select('id')->where('text', 'like', '%'.$text.'%')->first();
+            $result = DB::table('answers')->select('text')->where('question_id', '=', $IDQuestion)->first();
+
+
+            /*
             // Tableau de texte avec la séparation espace
             if (preg_match("[\s]", $text)) {
                 $array = explode(" ", $text);
@@ -31,7 +36,7 @@ class BotManController extends Controller
                 foreach ($array as $value) {
 
                     // Vérifie le texte par rapport à la base de donnée
-                    $query[] = DB::table('questions')->select('id')->where('text', 'like', '%'.$value.'%')->get();
+                    $query[] = DB::table('questions')->select('id')->where('text', 'like', '%'.$value.'%')->first();
                 }
 
                 $result = "Les réponses";
@@ -39,8 +44,9 @@ class BotManController extends Controller
                 $IDQuestion = DB::table('questions')->select('id')->where('text', 'like', '%'.$text.'%')->first();
                 $result = DB::table('answers')->select('text')->where('question_id', '=', $IDQuestion)->first();
             }
+            */
 
-            $bot->reply($result);
+            $bot->reply($result->text);
         });
 
         $botman->fallback(function($bot) {
