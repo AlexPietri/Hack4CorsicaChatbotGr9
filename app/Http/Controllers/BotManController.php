f559ed551008c08b1ca6bf4009b8bfb82f2c911e
Lastennet <lastennet.l@gmail.com>
2018-01-16 11:27:24 +0100

    Add API

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -42,7 +42,7 @@ class BotManController extends Controller
             $bot->reply($results);
         });
 
-        $botman->hears(' {text} ?', function (Botman $bot, $text) {
+        $botman->hears('- {text} ?', function (Botman $bot, $text) {
             $bot->types();
 
             // Tableau de texte avec la séparation espace
